269b383db25f0e9552898fefdf114be46ca33192
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2019-10-31 21:29:10 -0300

    heroku

---
+++ dist/server.js
@@ -0,0 +1,12 @@
+var restify = require('restify');
+
+var server = restify.createServer({
+    name: 'Lora App',
+    version: '1.0.0'
+});
+
+server.get('/*', restify.plugins.serveStaticFiles('./dist'));
+
+server.listen( process.env.PORT || 2000, function () {
+    console.log('%s running on %s', server.name, server.url);
+});
