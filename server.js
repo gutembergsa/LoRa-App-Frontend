1026fa2998f7bf43f04187d5415beaeab6b8c903
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2019-11-01 12:11:07 -0300

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import { createServer, plugins } from 'restify';
+const restify =  require('restify');
 
-var server = createServer({
+var server = restify.createServer({
     name: 'Lora App',
     version: '1.0.0'
 });
 
-server.get('/*', plugins.serveStaticFiles('./dist'));
+server.get('/*', restify.plugins.serveStaticFiles('./dist'));
 
 server.listen( process.env.PORT || 2000, function () {
     console.log('%s running on %s', server.name, server.url);
