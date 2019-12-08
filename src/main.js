b88b244501d80efe9e3d28fe45c38e50bf84d245
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2019-12-08 13:03:01 -0300

    remove comments

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import 'bulma/css/bulma.css'
 import App from './App.vue'
 import store from './store'
 import './registerServiceWorker'
 
-
+Vue.use(Buefy);
 Vue.config.productionTip = false
 
 Vue.use(Buefy);
